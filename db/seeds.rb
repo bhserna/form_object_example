cb6412bb91bb6bf4dc09525fe6d7e0140a2c7e91
bhserna <bhserna@gmail.com>
2021-12-27 08:03:54 -0600

    Seeds and some styles ont the transactions table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+
+(1..5).to_a.each do |i|
+  account = Account.create(name: "Account #{i}")
+
+  # inflows
+  [5, 4, 3].each do |days|
+    account.transactions.create(
+      date: days.days.ago,
+      payer_name: FFaker::Name.name,
+      description: FFaker::Lorem.phrase,
+      amount: 1_000
+    )
+  end
+end
